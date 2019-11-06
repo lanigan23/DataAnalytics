188d7b8dee69b830ecea2c04fc5d7f7fcb0214fb
Killian Lanigan <klanigan@tcd.ie>
2019-11-06 16:56:29 +0000

    Updated Lab 8

--- Lab8.R
+++ Lab8.R
@@ -1,18 +1,62 @@
 num.samples <- 2000
-U <- runif(num.samples)
+sample.size <- 2000
+#Q1
 lambda <- 1
-X <- -log(1-U)/lambda #sample
-h <- hist(X)
-xlines <- seq (min(h$breaks), max(h$breaks), length.out = 100)
-ylines <- dnorm(xlines, mean = mean(X), sd=sd(X))
-ylines <- ylines * diff(h$mids[1:2]*length(X))
-lines(xlines, ylines, lwd=2)
-ks.test(X, "pexp")
+samples <- list()
+for (i in 1:num.samples) {
+  exp_samples <- rep(NA, sample.size)
+  U <- runif(sample.size, 0, 1)
+  for (j in 1:sample.size) {
+    exp_samples[j] = -log(1-U[j]/lambda)
+  }
+  samples[[i]] <- exp_samples
+}
 
-X2 <- rgamma(num.samples, shape=5, scale = 3)
-h2 <- hist(X2)
-xlines <- seq (min(h2$breaks), max(h2$breaks), length.out = 100)
-ylines <- dnorm(xlines, mean = mean(X2), sd=sd(X2))
-ylines <- ylines * diff(h2$mids[1:2]*length(X2))
-lines(xlines, ylines, lwd=2)
-ks.test(X2, "gamma")
+pdfs <- list()
+for (i in 1:length(samples)) {
+  pdfs[[i]] <- dexp(samples[[i]], rate=lambda)
+}
+
+hist(exp_samples[[1]])
+plot(x=samples[[1]],y=pdfs[[1]], type="p")
+
+tests <- rep(NA, num.samples)
+for (i in 1:num.samples) {
+  ksresults <- ks.test(samples[[i]], "pexp")
+  tests[i] <- ksresults$p.value
+}
+
+mean(tests)
+min(tests)
+max(tests)
+
+#Q2
+k <- 5
+lambda <- 3
+gamma_samples <- list()
+for (i in 1:num.samples) {
+  sum <- rep(0, k)
+  index <- sample(1:num.samples, k)
+  for (j in 1:k) {
+    sum <- sum + samples[[index[j]]]
+  }
+  gamma_samples[[i]] <- sum
+}
+
+pdfs <- list()
+for (i in 1:length(gamma_samples)) {
+  pdfs[[i]] <- dgamma(gamma_samples[[i]], shape = k, rate = lambda)
+}
+
+hist(gamma_samples[[1]])
+plot(gamma_samples[[1]], pdfs[[1]], type = "p")
+
+tests <- rep(NA, num.samples)
+for (i in 1:num.samples) {
+  ksresults <- ks.test(gamma_samples[[i]], "pgamma", k, lambda)
+  tests[i] <- ksresults$p.value
+}
+
+mean(tests)
+min(tests)
+max(tests)
---
+++ Lab9.R
@@ -0,0 +1,9 @@
+f <- dgamma(5.5, 3)
+g <- dgamma(5, 2)
+M <- max(x/x2)
+h <- M*g
+x = rep(NA, 2000)
+u = rep(NA, 2000)
+for (i in 1:2000) {
+  x[i] = 
+}
