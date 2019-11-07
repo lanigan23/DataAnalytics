82f736fc7c6633d2219517e79197d8d59be3b372
Killian Lanigan <klanigan@tcd.ie>
2019-11-07 11:47:31 +0000

    Lab 9

--- Lab9.R
+++ Lab9.R
@@ -1,9 +1,31 @@
-f <- dgamma(5.5, 3)
-g <- dgamma(5, 2)
-M <- max(x/x2)
-h <- M*g
-x = rep(NA, 2000)
-u = rep(NA, 2000)
-for (i in 1:2000) {
-  x[i] = 
+num.samples <- 2000
+sample.size <- 2000
+f <- function(x) {
+  return(dgamma(x, 5.5, 3))
 }
+g <- function(x) {
+  return(dgamma(x, 5, 2))
+}
+M <- function(x) {
+  return(max(f(x)/g(x))) 
+}
+h <- function(M, g) {
+  return(M*g)
+}
+samples <- rep(NA, num.samples)
+x <- rep(NA, num.samples)
+u <- rep(NA, num.samples)
+index <- 1
+for (i in 1:num.samples) {
+  rv <- runif(1, 0, 10)
+  x[i] <- g(rv)
+  m <- M(rv)
+  u[i] <- runif(1, 0, h(m, x[i]))
+  if (u[i] <= f(x[i])) {
+    samples[index] <- x[i]
+    index <- index + 1
+  }
+}
+hist(samples)
+plot(samples, f(samples))
+ks.test(samples, "pgamma", 5.5 ,3)
