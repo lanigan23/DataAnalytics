13e243bbaa7ebd01297dce025b69657e8f318934
Killian Lanigan <klanigan@tcd.ie>
2019-11-08 14:05:37 +0000

    Updated Lab 9

--- Lab9.R
+++ Lab9.R
@@ -1,31 +1,39 @@
-num.samples <- 2000
-sample.size <- 2000
+num.samples <- 6
 f <- function(x) {
   return(dgamma(x, 5.5, 3))
 }
 g <- function(x) {
   return(dgamma(x, 5, 2))
 }
-M <- function(x) {
-  return(max(f(x)/g(x))) 
+M <- function(f, g) {
+  return(max(f/g)) 
 }
 h <- function(M, g) {
   return(M*g)
 }
-samples <- rep(NA, num.samples)
+samples <- rep(NA, 2000)
 x <- rep(NA, num.samples)
 u <- rep(NA, num.samples)
 index <- 1
-for (i in 1:num.samples) {
-  rv <- runif(1, 0, 10)
-  x[i] <- g(rv)
-  m <- M(rv)
-  u[i] <- runif(1, 0, h(m, x[i]))
-  if (u[i] <= f(x[i])) {
-    samples[index] <- x[i]
+i <- 0.01
+m <- 0
+while (i < num.samples) {
+  diff <- M(f(i), g(i))
+  if (m < diff) {
+    m <- diff
+  }
+  i <- i + 0.01
+}
+
+while (i < 2000) {
+  x <- runif(1, 0, 6)
+  xi <- g(x)
+  ui <- runif(1, 0, h(m, xi))
+  if (ui < f(x)){
+    samples[index] <- ui
     index <- index + 1
   }
+  i <- i + 1
 }
-hist(samples)
-plot(samples, f(samples))
-ks.test(samples, "pgamma", 5.5 ,3)
+
+ks.test(samples, "pgamma", 5.5, 3)
